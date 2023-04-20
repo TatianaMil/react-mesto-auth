4e59a1ca6b7bd11b96aed1ec312ae14f083c0272
TatianaMil <ftv9988.it@gmail.com>
2023-04-20 14:35:49 +0400

    for deploy, chanched router

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import App from "./components/App";
+// import "./index.css";
+// import { BrowserRouter } from "react-router-dom";
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <BrowserRouter>
+//     <App />
+//   </BrowserRouter>
+// );
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
 );
